1d0884b67bfefde127ca2ef38e42786ecf649809
AlejandroRM1810 <alejandrorm1810@gmail.com>
2022-01-26 11:44:45 +0100

    asdfasfs

---
+++ Programming for Everybody/2.ifywhile.py
@@ -0,0 +1,27 @@
+#creacion de variables
+x = 2
+y = 8
+n = 4
+
+#mostrar en pantalla numero + aumento de ese numero
+print (x)
+x = x + 2
+print (x)
+
+#condicional para mayor o menor de 10
+
+if y <= 10:
+    print ('Número pequeño')
+if y > 10:
+    print ('Número grande')
+
+print ('¿He acertado?')
+
+#bucle con while
+
+while n > 0:
+    print (n)
+    n = n - 1
+print ('Bucle terminado')
+
+
