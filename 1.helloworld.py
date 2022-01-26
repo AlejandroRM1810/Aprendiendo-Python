0090cb12d352152671a5802e199a39c8d7c5492d
AlejandroRM1810 <alejandrorm1810@gmail.com>
2022-01-26 11:45:19 +0100

    Ahora si

--- 1.helloworld.py
+++
@@ -1,8 +0,0 @@
-print('Hello world')
-
-# el sharp hace que la línea se vuelva comentario, las triples comillas hacen varais líneas comentario
-# aunque salgan en rojo
-
-''' bla bla bla bla 
-bla bla bla bla bla 
-vuelva varias lineas en comentario '''
